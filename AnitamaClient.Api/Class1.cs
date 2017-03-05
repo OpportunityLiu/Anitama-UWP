6b09d4813cf0d26cfa4f5e47e45598754038307d
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-03-05 14:59:23 +0800

    init

---
+++ AnitamaClient.Api/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnitamaClient.Api
+{
+    public class Class1
+    {
+    }
+}
